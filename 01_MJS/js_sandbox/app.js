ce8e209205e65aab4dd6e85bb5c0c2edd53d932e
a-zerty <ken@webwez.ca>
2021-02-26 23:42:25 -0500

    Mise en place Modern JavaScrip

---
+++ 01_MJS/js_sandbox/app.js
@@ -0,0 +1,118 @@
+
+// 6. Using the console
+// // Log to console
+// console.log('Hello World');
+// console.log(123);
+// console.log(true);
+// var greeting = 'Hello';
+// console.log(greeting);
+// console.log([1,2,3,4]);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
+
+// console.error('This is some error');
+// console.clear();
+// console.warn('This is a warning');
+// console.time('Hello');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+// console.timeEnd('Hello');
+// /*
+//   multi
+//   line
+//   comments
+// */
+
+// 7. Variables - var, let & const
+
+// var, let, const
+
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // Can not start with number
+
+// // Multi word vars
+// var firstName = 'John'; // Camel case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'Tom'; // Pascal case
+// var firstname;
+
+// LET
+// let name;
+// name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+
+// CONST
+// const name = 'John';
+// console.log(name);
+// Can not reassign
+// name = 'Sara';
+// Have to assign a value
+// const greeting;
+
+// const person = {
+//     name: 'John',
+//     age: 30
+//   }
+  
+//   person.name = 'Sara';
+//   person.age = 32;
+  
+//   // console.log(person);
+  
+//   const numbers = [1,2,3,4,5];
+//   numbers.push(6);
+  
+//   console.log(numbers);
+
+
+
+// Data Types in JavaScript
+  // PRIMITIVE
+
+// // String
+// const name = 'John Doe';
+
+// // Number
+// const age = 45;
+// // Boolean
+// const hasKids = true;
+
+// // Null
+// const car = null;
+
+// // Undefined
+// let test;
+// // Symbol
+// const sym = Symbol();
+// console.log(typeof sym)
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
+}
+
+const today = new Date();
+console.log(today);
+console.log(typeof today);
